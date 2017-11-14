c3e1df52288f69cd90d6270b71371ae75e5a8e2e
zhangle1 <867997639@qq.com>
2017-11-14 16:56:45 +0800

    第一次提交

---
+++ app/src/main/java/zhangle1/com/github/znewsgeek/MainActivity.java
@@ -0,0 +1,17 @@
+package zhangle1.com.github.znewsgeek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.AbsListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
